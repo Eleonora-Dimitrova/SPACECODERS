d33ecd8dab38b8da664d55c6b33b363c6adc0b35
Eleonora Dimitrova <elli.d.dimitrova@gmail.com>
2025-03-20 13:50:31 +0200

    insulin total

--- calculator_insulin.py
+++ calculator_insulin.py
@@ -15,3 +15,9 @@ def calculate():
     
     insulin_carbs = carbs / 10 * insulin_per_10g_carbs
     
+    if blood_sugar > 8:
+        insulin_for_sugar = (blood_sugar - 8) * 0.5 
+    else:
+        insulin_for_sugar = 0
+    
+    insulin = insulin_carbs + insulin_for_sugar
