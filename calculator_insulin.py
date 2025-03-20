f34c7919498a84a8b83dbc8b72fc6e325de87c86
Eleonora Dimitrova <elli.d.dimitrova@gmail.com>
2025-03-20 13:44:53 +0200

    calculating

--- calculator_insulin.py
+++ calculator_insulin.py
@@ -1,6 +1,17 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/calculate', methods=['POST'])
 
+
+def calculate():
+    data = request.get_json()
+
+    carbs = data.get('carbs')
+    blood_sugar = data.get('blood_sugar')
+
+    insulin_per_10g_carbs = 1
+    
+    insulin_carbs = carbs / 10 * insulin_per_10g_carbs
+    
