25fc19f8c38f15dc6c80f3198090671e93059353
Eleonora Dimitrova <elli.d.dimitrova@gmail.com>
2025-03-20 14:01:00 +0200

    ready app - v.1

--- calculator_insulin.py
+++ calculator_insulin.py
@@ -13,7 +13,7 @@ def calculate():
 
     insulin_per_10g_carbs = 1
     
-    insulin_carbs = carbs / 10 * insulin_per_10g_carbs
+    insulin_carbs = (carbs / 10) * insulin_per_10g_carbs
     
     if blood_sugar > 8:
         insulin_for_sugar = (blood_sugar - 8) * 0.5 
@@ -21,3 +21,13 @@ def calculate():
         insulin_for_sugar = 0
     
     insulin = insulin_carbs + insulin_for_sugar
+    
+    if insulin < 4:
+        return jsonify({"message": "Your insulin level is low. We recommend insulin injection."})
+    elif insulin > 8:
+        return jsonify({"message": "Your insulin level is high. Take fast-absorbing carbohydrates."})
+    else:
+        return jsonify({"message": "Your insulin level is optimal."})
+    
+if __name__ ==  "__main__":
+    app.run(debug=True)
